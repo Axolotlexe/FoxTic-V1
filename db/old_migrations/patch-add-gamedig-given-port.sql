c24dc6415f724b31acad5d1949ac3cb0485d8df1
Axolotl.exe <114876177+Axolotlexe@users.noreply.github.com>
2025-06-10 10:54:27 +0000

    update

---
+++ db/old_migrations/patch-add-gamedig-given-port.sql
@@ -0,0 +1,5 @@
+-- GameDig given port patch file (empty - GameDig support removed in FoxTic optimization)
+-- This file exists to maintain database migration consistency
+-- GameDig monitoring functionality has been completely removed from FoxTic
+
+-- No SQL operations needed as GameDig support is disabled
---
+++ db/old_migrations/patch-added-kafka-producer.sql
@@ -0,0 +1,5 @@
+-- Kafka producer patch file (empty - Kafka support removed in FoxTic optimization)
+-- This file exists to maintain database migration consistency
+-- Kafka monitoring functionality has been completely removed from FoxTic
+
+-- No SQL operations needed as Kafka support is disabled
